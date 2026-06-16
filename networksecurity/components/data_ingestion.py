534b0eb289a0a1e0178ed70fc95526f9af73b042
yas1423 <yashxsingh17@gmail.com>
2026-06-16 15:23:25 +0530

    Created DataIngestionConfig

---
+++ networksecurity/components/data_ingestion.py
@@ -0,0 +1,11 @@
+from networksecurity.Exception_handling.exception import CustomException
+from networksecurity.logging.logger import logging
+
+from networksecurity.entity.config_entity import DataIngestionConfig
+
+import os
+import sys
+import pymongo
+from typing import List
+from sklearn.model_selection import train_test_split
+
